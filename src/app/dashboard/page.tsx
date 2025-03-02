959843cd57ad52101b1f9400921b90973e5093ee
Dunsin <ayodejiabisuwa23@gmail.com>
2025-03-02 07:59:41 +0100

    added content for dashboard/page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Explore_ from "@/components/Explore";
+import Head from "@/components/Head";
+
+function Explore() {
+    return (
+        <div>
+            <Head title="Explore" />
+            <Explore_ />
+        </div>
+    );
+}
+
+export default Explore;
